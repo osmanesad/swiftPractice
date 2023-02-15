8565d58b4d69e748779b9d39e77fb49e9999bb80
G-0324D <byesad@gmail.com>
2023-02-15 21:24:53 +0300

    Create coffeeFlavors.swift
    Dizi örneği.

---
+++ coffeeFlavors.swift
@@ -0,0 +1,10 @@
+var coffeeFlavors: Set = ["Caramel","Mocha","Pumpkin Spice","Vanilla","Blueberry"]
+
+if coffeeFlavors.contains("Caramel") {
+    print("One Caramel coffee coming right up.")
+} else {
+    print("We don't serve that coffee flavor here.")
+}
+
+//coffeeFlavors.remove("Caramel")
+//print(coffeeFlavors)
